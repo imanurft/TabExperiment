45ed12903e1f0352c3b132404924b61f70fcd88a
imanurft <imanurfitriani@gmail.com>
2018-01-12 01:54:30 +0700

    Initial commit

---
+++ app/src/main/java/com/example/fanzy/tabexperiment/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.fanzy.tabexperiment;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.Switch;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //Create an instance of the tab layout from the view
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+
+        //Set the text for each tab
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_table1));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_table2));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.tab_table3));
+
+        //Set the tabs to fill the entire layout
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        final PagerAdapter adapter = new PagerAdapter
+                (getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+
+        //Setting a listener for clicks.
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    };
+    }
+
